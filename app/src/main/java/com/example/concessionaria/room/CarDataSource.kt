6f3511fa941a48f59a7ab700d28cca398d29d852
jivoc <4422822-PatoAssado@users.noreply.gitlab.com>
2022-12-21 00:34:01 -0300

    "correct updateCar"

--- app/src/main/java/com/example/concessionaria/room/CarDataSource.kt
+++ app/src/main/java/com/example/concessionaria/room/CarDataSource.kt
@@ -17,9 +17,9 @@ class CarDataSource(private val carDao: CarDao) {
         }
     }
 
-    /*suspend fun updateCar(isSold:Boolean,id:Long) {
+    suspend fun updateCar(isSold:Boolean,id:Long) {
         Dispatchers.IO.apply {
             carDao.updateCar(isSold,id)
         }
-    }*/
+    /
 }
